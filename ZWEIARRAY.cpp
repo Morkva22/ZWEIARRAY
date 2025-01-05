85d478b89d3fc9717aafa9581e40018e9c7c7374
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-05 17:52:25 +0200

    done1

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,6 +1,19 @@
+
 #include <iostream>
 using namespace std;
 void main()
 {
-    
+    const int SIZE = 3;
+    int arr[SIZE] = {1,2,3};
+    int sum = 0;
+    sum = arr[0] + arr[1] + arr[2];
+    cout<<"sum = "<< sum << endl;
+    int Arithmetic_mean = 0;
+    Arithmetic_mean = sum / SIZE;
+    cout << "Arithmetic_mean = " << Arithmetic_mean << endl;
+    int max = 0;
+    max = arr[2];
+    int min = 0;
+    min = arr[0];
+    cout<< "max = " << max <<"\n" << "min = "<< min << endl;
 }
