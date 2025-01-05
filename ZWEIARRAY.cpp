899c2ef144871c9769fa031070094080a826a158
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-05 23:59:53 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
-void main()
+
+int main()
 {
-    
+    const int ROWS = 2;
+    const int COWS = 2;
+    int arr[ROWS][COWS];
+    int gg;
+    cout << "endtter number" << endl;
+    cin >> gg;
+    arr[0][0]=gg;
+    for (int i  = 0; i < ROWS; i++){
+        for (int k = 0; k < COWS; k++){
+            if (i==0 && k == 0){
+                arr[i][k] = gg;
+            }
+            else
+            {
+                arr[i][k] = arr[i][k-1] * 2;
+            }
+        }
+    }
+    for (int i = 0; i < ROWS; i++)
+    {
+        for ( int k = 0; k < COWS; k++)
+        {
+            cout << arr[i][k] << " ";
+            
+        }
+        cout << endl;
+    }
 }
