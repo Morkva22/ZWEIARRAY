c3dac1b66bebefd8f46d79aa4311d1b44b817c78
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 15:40:56 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 void main()
 {
+    int a,b;
+    cout << "enter 2 numbd " << endl;
+    cin >> a;
+    cin >> b;
+    
+    int *pa = &a;
+    int *pb = &b;
+    
+    if (*pa > *pb)
+    {
+        cout << "this numb be the larger " << *pa<<  endl;
+    } else
+        {
+        cout << "this numb be the larger " << *pb<< endl;
+        }
     
 }
