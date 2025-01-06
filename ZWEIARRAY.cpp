2c1c3cd29bae5d7bb47977cf3c85533bfe86f5db
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 17:50:19 +0200

    an

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -6,5 +6,13 @@
 using namespace std;
 void main()
 {
-    
+    const int size = 3;
+    int arr[size] = {1,2,3};
+    int *pr = arr;
+    cout << "Before: " <<  *pr <<" "<<  *(pr+1) <<" "<<  *(pr + 2) << endl;
+    cout << "after ";
+    for (int i = 0; i < size; i++)
+    {
+        cout <<  *(pr + size - i - 1) << " ";
+    }
 }
