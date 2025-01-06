97a4668d64cc2bf420eb17bec802676eb9da096a
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 02:26:12 +0200

    idk how work left and right😭

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,42 +1,94 @@
+/*/
+Створіть двовимірний масив.
+Заповніть його випадковими числами та покажіть на екран.
+Користувач обирає кількість зсувів і положення (ліворуч, праворуч, вгору, вниз).
+Виконати зсув масиву і показати на екран отриманий результат. Зсув циклічний.
+Наприклад, якщо ми маємо такий масив:
+
+1 2 0 4 5 3
+
+4 5 3 9 0 1
+
+і користувач вибрав зсув на 2 розряди вправо, то ми отримаємо:
+
+5 3 1 2 0 4
+
+0 1 4 5 3 9
+
+
+/*/
 #include <iostream>
+#include <ctime>
 using namespace std;
-void main()
+
+int main()
 {
-    const int ROWS1 = 5;
-    const int COLS1 = 10;
-    const int ROWS2 = 5;
-    const int COLS2 = 5;
-    
-
-    int arr[ROWS1][COLS1];
-    int arr2[ROWS2][COLS2];
+    const int ROWS = 2;
+    const int COLS = 5;
+    int arr[ROWS][COLS];
     srand(time(0));
-
-
-    
-    for (int i =0; i < ROWS1; i++) {
-        for (int o =0; o < COLS1; o++){
-            arr[i][o] = rand()%2;
-          
-        }
-    }
-    cout << "arr2 5]10:" << endl;
-    for (int i = 0; i < ROWS1; ++i)
-    {
-        for (int j = 0; j < COLS1; ++j) {
-            cout << arr[i][j] << " ";
+    for (int i = 0; i < ROWS; i++) {
+        for (int m = 0; m < COLS; m++) {
+            arr[i][m] = rand() % 6;
+            cout << arr[i][m] << " ";
         }
         cout << endl;
     }
-    for (int i = 0; i < ROWS2; i++) {
-        for (int j = 0; j < COLS2; j++) {
-            arr2[i][j] = arr[i][2 *  j] + arr[i][2 * j + 1];
+    int retreat;
+    cout << "input the number of retreat: 1 - SHIFT UP 2 - SHIFT DOWN 3 - SHIFT LEFT 4 - SHIFT RIGHT" << endl;
+    cin >> retreat;
+
+    if (retreat == 1)
+    {
+        //up
+        for (int m = 0; m < COLS; m++)
+        {
+            int pro = arr[0][m];
+            arr[0][m] = arr[1][m];
+            arr[1][m] = pro;
         }
     }
-    cout << "arr2 5x5:" << endl;
-    for (int i = 0; i < ROWS2; i++) {
-        for (int j = 0; j < COLS2; j++) {
-            cout << arr2[i][j] << " ";
+        //dauwn
+        else if (retreat == 2)
+        {
+            for (int n = 0; n  < COLS; n++)
+            {
+                int pro = arr[0][n];
+                arr[0][n] = arr[1][n];
+                arr[1][n] = pro;
+            }
+        }
+        //left
+    else if (retreat == 3)
+    {
+        for (int  h = 0; h < ROWS; h++)
+        {
+            int pro =  arr[h][0];
+            for (int i = 0; i < COLS - 1; i++)
+            {
+                arr[h][i] = arr[h][i + 1];
+            }
+            arr[h][COLS - 1] = pro;
+        }
+    }
+        //right
+    else if( retreat == 4)
+    {
+        for (int j = 0; j < ROWS; j++)
+        {
+            int pro = arr[j][COLS - 1];
+            for (int i = COLS - 1; i > 0; i--)
+            {
+                arr[j][i] = arr[j][i - 1];
+            }
+            arr[j][0] = pro;
+        }
+    }
+    
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int m = 0; m < COLS; m++) {
+            cout << arr[i][m] << " ";
         }
         cout << endl;
     }
