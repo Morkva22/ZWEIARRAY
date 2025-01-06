302f754813a840d0da0e2160391a50505bcc5c5d
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 15:52:24 +0200

    bloob has done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,20 @@
-/*/Використовуючи покажчики та оператор розіменування, визначити знак числа, введеного з клавіатури
- *
+/*/
+ Використовуючи покажчики та оператор розіменування, обміняти місцями значення двох змінних.
 /*/
 #include <iostream>
 using namespace std;
 void main()
 {
-
+ int a;
+ int b;
+ cout << "enter your two variant of variors a & b - ";
+ cin >> a;
+ cin >> b;
+ int *pa = &a;
+ int *pb = &b;
+ int pro = *pa;
+ *pa = *pb;
+ *pb = pro;
+ cout << "HAHAHAHA I HAVE REVERSED YOUR NUMBERS, AND NOW, A BE " << *pa << " AND B BE " << *pb<< endl;
+ 
 }
