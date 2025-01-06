8cce58be326ed9ad57264421c9ca09f4820d747a
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 15:45:02 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,6 +1,24 @@
+/*/Використовуючи покажчики та оператор розіменування, визначити знак числа, введеного з клавіатури
+/*/
 #include <iostream>
 using namespace std;
 void main()
 {
+    int a;
+    cout<<"Enter a number: ";
+    cin >> a;
+    int *pa = &a;
+    if (*pa > 0)
+    {
+        cout << "+";
+    }
+    else if(*pa<0)
+    {
+        cout << "-";
+        }
+        else if (*pa ==0)
+        {
+            cout<< " it is ABSOLUT ZERO...";
+        }
     
 }
