45be428473aea3a8cec88352107d5c17e502a6e6
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-06 17:43:08 +0200

    dg

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,18 @@
 /*/
+Використовуючи покажчик на масив цілих чисел, змінити порядок проходження елементів масиву на протилежний.
 
 /*/
 #include <iostream>
 using namespace std;
 void main()
 {
-    
+    const int size = 3;
+    int arr[size] = {1,2,3};
+    int *p = arr;
+    cout << "Before: " <<  *p << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<"after " << *(p - i)<<endl;
+        
+    }
 }
