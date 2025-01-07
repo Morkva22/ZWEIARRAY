47fbd2fc6488f44f1ea7d3f8d976bb3745452b8c
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-07 19:58:10 +0200

    d

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -37,3 +37,84 @@ void Delete_array(int *&arr)
     delete[] arr;
     arr = nullptr;
 }
+
+void push_back(int *&arr, int &size, const int value, int index) {
+    int *NewArray = new int[size+1];
+    for (int i = 0; i <= size; i++) {
+        if (i < index) {
+            NewArray[i] = arr[i];
+        }
+        if (i == index) {
+            NewArray[i] = value;
+        }
+        if (i > index) {
+            NewArray[i] = arr[i - 1];
+        }
+    }
+    size++;
+    delete[] arr;
+    arr = NewArray;
+}
+
+void pop_back(int *&arr, int &size, int index) {	
+    int *NewArray = new int[size-1];
+    for (int i = 0; i < size; i++) {
+        if (i < index) {
+            NewArray[i] = arr[i];
+        }
+        if (i > index) {
+            NewArray[i-1] = arr[i];
+        }
+    }
+    delete[] arr;
+    size--;
+    arr = NewArray;
+}
+void push_in(int *&arr, int &size, const int value)
+{
+    int *newArray = new int[size + 1];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = arr[i];
+    }
+    newArray[size++] = value;
+ 
+    delete[] arr;
+    arr = newArray;
+}
+int main()
+{
+    int size = 5;
+    int *arr = nullptr;
+
+    
+    memory_allocated(arr, size);//// vydelenie pamyati
+    initialized_array(arr, size);/// inicializaciya
+    ShowArray(arr, size); ///vivod
+    cout << endl;
+
+
+    
+    push_back(arr, size, 10); ///v kinez
+    ShowArray(arr, size);///vivod
+    cout << endl;
+
+
+    
+    push_back(arr, size, 20, 2);///vstavka
+    ShowArray(arr, size);///vivod
+    cout << endl;
+
+
+    
+    pop_back(arr, size, 2);///udalenie
+    ShowArray(arr, size);///vivod
+    cout << endl;
+
+
+    
+    Delete_array(arr);///udalenie massiva
+}
+
+
+
