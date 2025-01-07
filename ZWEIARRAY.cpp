a1a84ed08fb6bc17c761ec274f655b0154c121c7
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-07 14:58:40 +0200

    1

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,44 +1,10 @@
 /*/
 Дано два масиви: А[M] і B[N] (M і N вводяться з клавіатури).
-Необхідно створити третій масив мінімально можливого розміру,
-в якому потрібно зібрати спільні елементи двох масивів без повторень.
+Необхідно створити третій масив мінімально можливого розміру, у якому потрібно зібрати елементи обох масивів.
 
 /*/
 #include <iostream>
 using namespace std;
-
-void equalarrays(int *A, int *B, int *C, int M, int N, int &index) {
-    index = 0;
-    for (int i = 0; i < M; i++) {
-        bool uniq = true;
-        for (int j = 0; j < index; j++) {
-            if (C[j] == A[i]) {
-                uniq = false;
-            }
-        }
-        if (uniq) {
-            C[index++] = A[i];
-        }
-    }
-
-    for (int i = 0; i < N; i++) {
-        bool uniq = true;
-        for (int j = 0; j < index; j++) {
-            if (C[j] == B[i]) {
-                uniq = false;
-            }
-        }
-        if (uniq) {
-            C[index++] = B[i];
-        }
-    }
-    cout << "New array C: " << endl;
-    for (int i = 0; i < index; i++) {
-        cout << C[i] << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     int M, N;
     cout << "Enter the size M: ";
@@ -61,7 +27,6 @@ int main() {
     }
 
     int index;
-    equalarrays(A, B, C, M, N, index);
 
     delete[] A;
     delete[] B;
