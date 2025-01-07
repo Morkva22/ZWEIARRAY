603e3f4a3688d466ed7a5f0e1b10a17b20b7bc4d
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-07 19:48:04 +0200

    d

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,39 @@
 /*/
+Написати такі функції для роботи з динамічним масивом:
 
+Функція розподілу динамічної пам'яті.
+Функція ініціалізації динамічного масиву.
+Функція друку динамічного масиву.
+Функція видалення динамічного масиву.
+Функція додавання елемента в кінець масиву.
+Функція вставки елемента за вказаним індексом.
+Функція видалення елемента за вказаним індексом.
 /*/
 #include <iostream>
 using namespace std;
-void main()
+
+
+void memory_allocated(int *&arr , int size)
 {
-    
+    arr = new int[size];
+}
+
+void initialized_array(int *arr, int size)
+{
+    for (int i = 0; i<size; i++)
+    {
+        arr[i] = 0;
+    }
+}
+void ShowArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+void Delete_array(int *&arr)
+{
+    delete[] arr;
+    arr = nullptr;
 }
