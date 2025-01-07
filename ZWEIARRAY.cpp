1f0476b6c31554e32454eb413992981bbc93447e
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-07 14:57:45 +0200

    ddone....

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -3,8 +3,65 @@
 Необхідно створити третій масив мінімально можливого розміру, в якому потрібно зібрати спільні елементи двох масивів без повторень.
 /*/
 #include <iostream>
-using namespace std;
-void main()
-{
-    
-}
+    using namespace std;
+
+    void equalarrays(int *A, int *B, int *C, int M, int N, int &index) {
+        index = 0;
+        for (int i = 0; i < M; i++) {
+            bool uniq = true;
+            for (int j = 0; j < index; j++) {
+                if (C[j] == A[i]) {
+                    uniq = false;
+                }
+            }
+            if (uniq) {
+                C[index++] = A[i];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            bool uniq = true;
+            for (int j = 0; j < index; j++) {
+                if (C[j] == B[i]) {
+                    uniq = false;
+                }
+            }
+            if (uniq) {
+                C[index++] = B[i];
+            }
+        }
+        cout << "New array C: " << endl;
+        for (int i = 0; i < index; i++) {
+            cout << C[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int main() {
+        int M, N;
+        cout << "Enter the size M: ";
+        cin >> M;
+        cout << "Enter the size N: ";
+        cin >> N;
+
+        int* A = new int[M];
+        int* B = new int[N];
+        int* C = new int[M + N];
+
+        cout << "Input numbers in A:" << endl;
+        for (int i = 0; i < M; i++) {
+            cin >> A[i];
+        }
+
+        cout << "Input numbers in B:" << endl;
+        for (int i = 0; i < N; i++) {
+            cin >> B[i];
+        }
+
+        int index;
+        equalarrays(A, B, C, M, N, index);
+
+        delete[] A;
+        delete[] B;
+        delete[] C;
+    }
