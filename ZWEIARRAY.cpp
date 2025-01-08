406966535db0cf7fa669b87bd667b00350332c24
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-08 12:18:17 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,17 @@
 /*/
-
+Дано рядок символів. Замінити в ньому всі пробіли на табуляції.
 /*/
 #include <iostream>
 using namespace std;
 void main()
 {
-    
+    char string[] = "Str ing wi th wo rds\0";
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (string[i] == ' ')
+        {
+            string[i] = '\t';
+        }
+    }
+    cout << string << endl;
 }
