9d3cf4dfe16b5c2e7bdd8170eaa04f694d386ebc
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-10 20:26:31 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,34 @@
 /*/
-
+Дано рядок символів. Визначити кількість літер, цифр та інших символів, присутніх у рядку.
 /*/
 #include <iostream>
+#include <cstring>
 using namespace std;
-void main()
-{
-    
+
+void countSymbols(const char *str, int &letters, int &numbers, int &others) {
+     letters = 0;
+     numbers = 0;
+     others = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            letters++;
+        } else if (str[i] >= '0' && str[i] <= '9') {
+            numbers++;
+        } else {
+            others++;
+        }
+    }
+}
+
+int main() {
+    const char *str = "hello 228!)) . . .";
+    int letters, numbers, others;
+    countSymbols(str, letters, numbers, others);
+
+    cout << "Letters: " << letters << endl;
+    cout << "Numbers: " << numbers << endl;
+    cout << "Others: " << others << endl;
+
+    return 0;
 }
