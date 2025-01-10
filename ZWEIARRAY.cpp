3e5e0aac58b676b9ae1b7424a127015d7617854c
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-11 01:26:28 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -4,7 +4,26 @@
 /*/
 #include <iostream>
 using namespace std;
+
+void switchdots (char *str, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '.')
+        {
+            str[i] = '!';
+        }
+    }
+}
+
+
 void main()
 {
-    
+    int n;
+    cout << "Enter string with dotas: ";
+    char str[999];
+    cin >> str;
+    n = strlen(str);
+    switchdots(str, n);
+    cout << "String with '.' was replaced by '!' : " << str << endl;
 }
