73d7180af7193881272052f8ce32c3bd271132a1
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-11 01:41:01 +0200

    donent

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,27 @@
 /*/
-
+Користувач вводить рядок символів і шуканий символ.
+Порахувати скільки разів він зустрічається в рядку.
 /*/
 #include <iostream>
+#include <cstring>
 using namespace std;
-void main()
-{
-    
+
+void charsearch(const char *str, int &n, char &g) {
+    n = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == g) {
+            n++;
+        }
+    }
+}
+int main() {
+    int n;
+    char g;
+    char str[999];
+    cout << "Enter string: ";
+    cin.getline(str, 999);
+    cout << "And what symbol do you need? ";
+    cin >> g;
+    charsearch(str, n, g);
+    cout << "The symbol '" << g << "' found " << n << " times" << endl;
 }
