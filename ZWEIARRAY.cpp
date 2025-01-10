2903d7f9c1d717f8548b424d279fcdea1e7c5afd
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-10 20:34:49 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,23 @@
 /*/
-
+Підрахувати кількість слів у введеному реченні.
 /*/
 #include <iostream>
 using namespace std;
+
+
+int countsymbols(const char *str)
+{
+    int count = 0;
+    while (str[count]!= '\0')
+    {
+        count++;
+    }
+    return count;
+}
+
 void main()
 {
-    
+   const char *str = "Schwerer Panzerspähwagen Sonderkraftfahrzeug 234";
+    cout << countsymbols(str) << endl;
 }
+
