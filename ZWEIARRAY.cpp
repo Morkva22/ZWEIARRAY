c208b13b3de35ed35148b1b0aeb07ef7ffcc866e
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-11 16:40:06 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,21 @@
 /*/
-
+Написати рекурсивну функцію знаходження ступеня числа.
 /*/
 #include <iostream>
 using namespace std;
+
+int power(int a, int n)
+{
+    if (n == 0)
+        return 1;
+    return a * power(a, n - 1);
+}
 void main()
 {
-    
+    int a, n;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << a << "^" << n << " = " << power(a, n) << endl;
 }
