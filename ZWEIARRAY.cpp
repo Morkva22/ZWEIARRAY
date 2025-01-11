97cd8b467fabdbac2cc12e5b8547b63c252282eb
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-11 15:40:56 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,21 @@
 /*/
-
+написати функцію, що визначає середнє арифметичне елементів переданого їй масиву.
 /*/
 #include <iostream>
 using namespace std;
+
+void avarage(int arr[], int size)
+{
+    int sum  = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    cout << "Avarage: " << sum / size << endl;
+    
+}
 void main()
 {
-    
+    int arr[] = {1, 2, 3, 4, 5};
+    avarage(arr, 5);
 }
