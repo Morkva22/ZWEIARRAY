010ae6bba2c1067da4d853467d310cdd9aad76d2
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-13 16:55:01 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -1,9 +1,20 @@
 /*/
-
+Написати рекурсивну функцію,яка виводить N зірок у ряд,
+число N задає користувач. Проілюструйте роботу функції прикладом.
 /*/
 #include <iostream>
 using namespace std;
-void main()
-{
-    
+void star(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << "*";
+    star(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of stars: ";
+    cin >> n;
+    star(n);
 }
