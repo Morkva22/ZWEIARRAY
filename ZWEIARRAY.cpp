07f13e26d5aef00aec7827402695e506ecf0f324
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-18 15:50:55 +0200

    done

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -4,10 +4,18 @@
 #include <iostream>
 using namespace std;
 
+int cm(int a, int b) {
+    if (b == 0)
+        return a;
+    return cm(b, a % b);
+}
 
+int main() {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
 
+    cout << "Your cm" << num1 << " and " << num2 << " is " << cm(num1, num2) << endl;
 
-void main()
-{
-    
+    return 0;
 }
