a3d5fbd2abc5edc761d870278775838174c49dac
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-21 23:54:19 +0200

    done it

--- ZWEIARRAY.cpp
+++ ZWEIARRAY.cpp
@@ -9,14 +9,67 @@
 using namespace std;
 
 
-void Retaking()
-{
-    
+
+void ShowArray(int* arr, int marks) {
+    for (int i = 0; i < marks; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+void pop_back(int *&arr, int &marks, int index) {	
+    int *NewArray = new int[marks-1];
+    for (int i = 0; i < marks; i++) {
+        if (i < index) {
+            NewArray[i] = arr[i];
+        }
+        if (i > index) {
+            NewArray[i-1] = arr[i];
+        }
+    }
+    delete[] arr;
+    marks--;
+    arr = NewArray;
 }
 
 
+void push_back(int *&arr, int &marks, int value, int index) {
+    int *NewArray = new int[marks+1];
+    for (int i = 0; i <= marks; i++) {
+        if (i < index) {
+            NewArray[i] = arr[i];
+        }
+        if (i == index) {
+            NewArray[i] = value;
+        }
+        if (i > index) {
+            NewArray[i] = arr[i - 1];
+        }
+    }
+    marks++;
+    delete[] arr;
+    arr = NewArray;
+}
 
-void menu(const int* marks[],const int* arr)
+
+void Scholarship(int* arr, int marks)
+{
+    int sum = 0;
+    for (int i = 0; i < marks; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum / marks >= 10.7)
+    {
+        cout  << "\tHe have a scholarship" << endl;
+    }
+    else
+    {
+        cout << "\tHe dont have a scholarship(" << endl;
+    }
+}
+
+
+void menu(int marks, int* arr)
 {
     int choose = 0;
     cout << "Choose the option" << endl;
@@ -29,10 +82,7 @@ void menu(const int* marks[],const int* arr)
     if (choose == 1)
     {
         
-        for (int i = 0; i < 10; i++)
-        {
-            cout << marks[i] << " ";
-        }
+        ShowArray(arr,marks);
         cout << endl;
         cout << "Press '4' to exit in menu\t" << endl;
         cin>>choose;
@@ -43,8 +93,19 @@ void menu(const int* marks[],const int* arr)
     }
     if (choose == 2)
     {
-        SortsHomeN(HomeArr);
+        int value, index;
+        ShowArray(arr, marks);
+        cout<<endl;
+        cout << "Name the index of the mark what need to clear(start count with 0)" << endl;
+        cin >> index;
+        pop_back(arr, marks, index);
+        cout << "\n";
+        cout << "and the new mark" << endl;
+        cin >> value;
         cout  << endl;
+        push_back(arr, marks, value, index);
+        ShowArray(arr, marks);
+        cout<<endl;
         cout << "Press '4' to exit in menu\t" << endl;
         cin>>choose;
         if (choose == 4)
@@ -54,7 +115,7 @@ void menu(const int* marks[],const int* arr)
     }
     if (choose == 3)
     {
-        Users(MobileArr, HomeArr, 5);
+        Scholarship(arr, marks);
         cout << endl;
         cout << "Press '4' to exit in menu\t" << endl;
         cin>>choose;
@@ -70,13 +131,20 @@ void menu(const int* marks[],const int* arr)
     else
     {
         cout << "Choose the correct option" << endl;
+        cout <<endl << endl << endl;
+        menu(marks, arr);
     }
 }
 
 
 void main()
 {
-    int marks[10];
-    int arr[marks]{9,7,10,8,12,11,9,6,11,11};
+    int marks=10;;
+    int* arr = new int [marks]{9,7,10,8,12,11,9,6,11,11};
     
+menu(marks, arr);
+    
+    delete[] arr;
 }
+
+
